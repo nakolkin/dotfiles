bf59da2410da707a19c4c739888388d38e661327
Nikolaenko, Artem <Artem.Nikolaenko@neustar.biz>
2016-01-18 16:48:34 +0200

    latest

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -211,3 +211,8 @@ alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
 alias brewu='brew update  && brew upgrade && brew cleanup && brew prune && brew doctor'
+
+# Mine 
+
+alias v='f -e vim'  # quick opening files with vim
+alias e='f -e subl'  # quick opening files with sublime
