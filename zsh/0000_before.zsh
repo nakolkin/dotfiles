5e6250ac3e52128da017b57ff0a224fd36227955
Nikolaenko, Artem <Artem.Nikolaenko@team.neustar>
2017-10-10 14:31:25 +0300

    some updates

--- zsh/0000_before.zsh
+++ zsh/0000_before.zsh
@@ -5,3 +5,8 @@ if [ -d $HOME/.zsh.before/ ]; then
     for config_file ($HOME/.zsh.before/*.zsh) source $config_file
   fi
 fi
+
+#szh completions
+source /usr/local/share/zsh/site-functions/_aws
+source /usr/local/share/zsh/site-functions/_docker
+source /usr/local/share/zsh/site-functions/_docker-compose
