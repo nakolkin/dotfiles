dd9533893d03e10d22a0d12a11ff851206e03b63
Nikolaenko, Artem <Artem.Nikolaenko@team.neustar>
2017-10-10 14:31:25 +0300

    some updates

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -12,3 +12,12 @@ PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS;
 
 pathAppend "$HOME/.yadr/bin"
 pathAppend "$HOME/.yadr/bin/yadr"
+
+#custom
+pathAppend "$HOME/.rvm/bin" # Add RVM to PATH for scripting
+pathAppend "$HOME/devel/mip-db/docker_scripts" #docker specific script
+pathAppend "/usr/local/opt/mysql@5.6/bin"
+
+export JAVA_HOME="`/usr/libexec/java_home -v '1.8*'`"
+#export PHANTOMJS_BIN=/usr/local/Cellar/phantomjs/1.9.8/bin
+export SPARK_HOME=/Users/tema/devel/spark-2.1.0 
